50bd621f236dae7da6bd28467c830b2e967e3072
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-10-07 11:12:48 +0200

    make it better

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -1,7 +1,25 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "987654321",
-    DB: "doitnow"
-  };
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "987654321",
+//     DB: "doitnow"
+//   };
   
+
+/*
+
+Connection from Clear DB from heroku Server
+
+  //mysql:
+  //b7f446ef2f45cd:6c97e5a6@eu-cdbr-west-01.cleardb.com
+  /heroku_055097fa390c2ee?
+  reconnect=true
+
+*/
+
+module.exports = {
+  HOST: "eu-cdbr-west-01.cleardb.com",
+  USER: "b7f446ef2f45cd",
+  PASSWORD: "6c97e5a6",
+  DB: "heroku_055097fa390c2ee"
+};
